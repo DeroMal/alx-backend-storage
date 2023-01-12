1ee900bbdbfb3ddf613614eaf95ac5db53f7e52d
Derrick L. Mayiku <40387583+DeroMal@users.noreply.github.com>
2023-01-12 19:27:05 +0300

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,4 @@
--- Select origin, sum of nb_fans from metal_bands table, group them by origin and order them by sum of nb_fans in descending order
-SELECT origin, SUM(nb_fans) as nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
-
+-- SQL script that ranks
+SELECT DISTINCT `origin`, SUM(`fans`) as `nb_fans` FROM `metal_bands`
+GROUP BY `origin`
+ORDER BY `nb_fans` DESC;
