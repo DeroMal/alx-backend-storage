d40213b1a5cfbc7494371b303fe398615839cc01
DeroMal <derrickmla123@gmail.com>
2023-01-17 22:19:52 +0300

    fr

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def top_students(mongo_collection):
+    """
+    Returns all students sorted by average score.
+    :param mongo_collection: pymongo collection object
+    :return: List of students documents with average score
+    """
+    pipeline = [
+        {"$unwind": "$scores"},
+        {"$group": {"_id": "$_id", "averageScore": {"$avg": "$scores.score"}}},
+        {"$sort": {"averageScore": -1}}
+    ]
+    cursor = mongo_collection.aggregate(pipeline)
+    return list(cursor)
