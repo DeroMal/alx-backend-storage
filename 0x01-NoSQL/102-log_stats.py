f5cb1b99b8c25415eb02ee1d96aedf4d31c82f1d
Derrick L. Mayiku <40387583+DeroMal@users.noreply.github.com>
2023-01-17 22:34:54 +0300

    Update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
 """102-log_stats.py"""
-from pymongo import MongoClient
 
-def top_students(mongo_collection):
+def top_students(mongo_collection, nginx_collection):
     """
     Returns all students sorted by average score and the top 10 most present IPs in the "nginx" collection of the "logs" database.
-    :param mongo_collection: pymongo collection object
+    :param mongo_collection: pymongo collection object for students
+    :param nginx_collection: pymongo collection object for nginx logs
     :return: List of students documents with average score and a list of the top 10 most present IPs
     """
-    students_pipeline = [
-        {"$unwind": "$scores"},
-        {"$group": {"_id": "$_id", "averageScore": {"$avg": "$scores.score"}}},
-        {"$sort": {"averageScore": -1}}
-    ]
-    students_cursor = mongo_collection.aggregate(students_pipeline)
+    students_cursor = mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
     students_list = list(students_cursor)
-    client = MongoClient()
-    nginx_collection = client.logs.nginx
-    ip_top_pipeline = [
+    ip_top_cursor = nginx_collection.aggregate([
         {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
         {"$sort": {"count": -1}},
         {"$limit": 10}
-    ]
-    ip_top_cursor = nginx_collection.aggregate(ip_top_pipeline)
+    ])
     ip_top_list = list(ip_top_cursor)
     return (students_list, ip_top_list)
