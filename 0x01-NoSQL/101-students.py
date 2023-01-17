23d75cc7ba029e79dbf38309f1522f729d98db92
Derrick L. Mayiku <40387583+DeroMal@users.noreply.github.com>
2023-01-17 22:30:09 +0300

    Update 101-students.py

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 """101-students.py"""
+
 def top_students(mongo_collection):
-    
-    """
-    Returns all students sorted by average score.
-    :param mongo_collection: pymongo collection object
-    :return: List of students documents with average score
-    """
-    pipeline = [
-        {"$unwind": "$scores"},
-        {"$group": {"_id": "$_id", "averageScore": {"$avg": "$scores.score"}}},
-        {"$sort": {"averageScore": -1}}
-    ]
-    cursor = mongo_collection.aggregate(pipeline)
-    return list(cursor)
+    """returns all students sorted by average score"""
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
